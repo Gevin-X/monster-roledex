c36feae4060f986b72b659aea992e6e71c09c093
gevin-x <gemba.gp@gmail.com>
2022-03-14 01:15:05 +0530

    Updates

---
+++ precache-manifest.dcdfba47c42d68a66db439abdc9fb182.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c522a1918a1d9e0e68bedd028554e6",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "39f03f60e6aa9b9c70bf",
+    "url": "/monster-roledex/static/css/main.3aae8a3f.chunk.css"
+  },
+  {
+    "revision": "37f373e3614c14250ae5",
+    "url": "/monster-roledex/static/js/2.729e6090.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster-roledex/static/js/2.729e6090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f3488441f61b183a6d",
+    "url": "/monster-roledex/static/js/3.cfd6da00.chunk.js"
+  },
+  {
+    "revision": "39f03f60e6aa9b9c70bf",
+    "url": "/monster-roledex/static/js/main.2badb4b1.chunk.js"
+  },
+  {
+    "revision": "65ccb779610bc6259264",
+    "url": "/monster-roledex/static/js/runtime-main.f9d4c490.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/monster-roledex/static/media/logo.06e73328.svg"
+  }
+]);
